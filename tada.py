7ef8e0a8a0a88525decfe1fc05fd05e7e86b1456
Mukund Lakshman <yaymukund@gmail.com>
2014-05-26 18:48:40 -0400

    Okay, let this server just serve the app.

--- tada.py
+++ tada.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder = 'static', static_url_path = '')
 os.environ.setdefault('TADA_SETTINGS_PATH', 'config/development.py')
 app.config.from_object('config.shared')
 app.config.from_envvar('TADA_SETTINGS_PATH')
@@ -14,3 +14,7 @@ app.register_blueprint(blueprint)
 @app.errorhandler(404)
 def not_found(error):
     return jsonify(error = '404 Not Found'), 404
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
